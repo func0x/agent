83920523eb6423910c78cd1c91a32269f366aee2
Damian Janus <damian.janus@gmail.com>
2026-01-23 12:29:11 +0100

    add function_call and fix run_python_file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -18,7 +18,7 @@ def run_python_file(working_directory, file_path, args=None):
         if not file_path.endswith(".py"):
             return f'Error: "{file_path}" is not a Python file'
 
-        command = ["python", working_dir_abs]
+        command = ["python", target_file_abs]
         if args:
             command.extend(args)
 
